72bbc8a7b6f5376ed66ba287b5f57811e32eca0f
Iurie Muradu <muradu.iurie.1986@gmail.com>
2025-07-07 12:37:07 +0300

    first commit

---
+++ api/api.py
@@ -0,0 +1,15 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(e):
+    return {'error': 'Not Found'}, 404
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
+
+# if __name__ == "__main__":
+#     app.run()
---
+++ frontend/precache-manifest.b04d4842da0a1b4b203e30a7c272fd77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4f1514b40e91c9175fb12e42e9afe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f65d782208dd5990f54f",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "eb48b8235cc4eba7f200",
+    "url": "/static/js/2.741504f9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.741504f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65d782208dd5990f54f",
+    "url": "/static/js/main.992952f0.chunk.js"
+  },
+  {
+    "revision": "4be130620b223c1c9c69",
+    "url": "/static/js/runtime-main.1cbfc7dc.js"
+  }
+]);
