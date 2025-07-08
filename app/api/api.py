8dcdc01e8651d5f74bc03126e2d4c0c955dfe82d
Iurie Muradu <muradu.iurie.1986@gmail.com>
2025-07-08 19:27:38 +0300

    revert api code to original

--- app/api/api.py
+++ app/api/api.py
@@ -1,24 +1,7 @@
 import time
 from flask import Flask
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.exporter.prometheus import PrometheusMetricReader
-from opentelemetry.metrics import set_meter_provider  # Important!
-from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 
 app = Flask(__name__)
-
-# Define OTEL resource (service name)
-resource = Resource.create(attributes={"service.name": "dataiku-time-service"})
-
-# Setup Prometheus metric reader + meter provider
-reader = PrometheusMetricReader()
-provider = MeterProvider(resource=resource, metric_readers=[reader])
-set_meter_provider(provider)
-
-# Instrument Flask app (includes HTTP metrics!)
-FlaskInstrumentor().instrument_app(app)
 
 @app.errorhandler(404)
 def not_found(e):
@@ -27,10 +10,3 @@ def not_found(e):
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
-
-@app.route("/metrics")
-def metrics():
-    return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
-
-if __name__ == "__main__":
-    app.run()
